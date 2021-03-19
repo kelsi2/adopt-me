952950ae4f19f51bc8f1230d83192abeb281460f
Kelsi Proulx <Kelsiproulx1919@gmail.com>
2021-03-19 14:37:51 -0700

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
-  ])
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement(
@@ -12,25 +8,26 @@ const App = () => {
     // We can pass arguments here (id, class, etc.)
     {},
     [
-      React.createElement('h1', { id: 'something-important' }, "Adopt Me!"),
+      React.createElement("h1", { id: "something-important" }, "Adopt Me!"),
       // No attributes or children, these are optional
-      React.createElement(Pet, { 
-        name: 'Hermione', 
-        animal: 'Cat', 
-        breed: 'Domestic short-hair' 
+      React.createElement(Pet, {
+        name: "Hermione",
+        animal: "Cat",
+        breed: "Domestic short-hair",
       }),
-      React.createElement(Pet, { 
-        name: 'Pepper', 
-        animal: 'Bird', 
-        breed: 'Cockatiel' 
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel",
       }),
-      React.createElement(Pet, { 
-        name: 'Luna', 
-        animal: 'Dog', 
-        breed: 'Havanese' 
-      })
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese",
+      }),
     ]
-  )
-}
+  );
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+// We imported render so don't need to say ReactDOM.render
+render(React.createElement(App), document.getElementById("root"));
