266416f8bb738b862aa6f63cdbe7c9472019be57
Kelsi Proulx <Kelsiproulx1919@gmail.com>
2021-03-24 11:05:32 -0700

    Add external colors

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
+import colors from "./colors";
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo" role="img">
-      🐶
-    </span>
-  </header>
-);
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const NavBar = () => {
+  const [padding] = useState(15);
+
+  return (
+    <header
+      css={css`
+        background-color: ${colors.secondary};
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
+
+          &:hover {
+            animation: 1s ${spin} linear infinite reverse;
+            text-decoration: underline;
+          }
+        `}
+        aria-label="logo"
+        role="img"
+      >
+        🐶
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
